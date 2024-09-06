d326cc0571eb1d89bb9468d9c2befff4ef0bcd8d
ParadoxBrother <48399901+ParadoxBrother@users.noreply.github.com>
2024-09-06 04:43:20 -0700

    Updated README.md

--- app/generate/page.js
+++ app/generate/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { collection, setDoc, doc, getDoc, writeBatch } from 'firebase/firestore';
-import db from '@/firebase';
+import { db } from '@/firebase';
 import {
   Container,
   TextField,
